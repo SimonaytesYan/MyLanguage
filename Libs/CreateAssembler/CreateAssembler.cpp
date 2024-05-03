6267eabf5dde1e07833460baad1122a0f37b1629
Simonaytes <yandexman228@gmail.com>
2024-05-03 17:31:03 +0300

    Fix some

--- Libs/CreateAssembler/CreateAssembler.cpp
+++ Libs/CreateAssembler/CreateAssembler.cpp
@@ -548,12 +548,14 @@ int  PutCall(Node* node, FILE* output_file)
 
     if (strcmp(VAL_FUNC(node), "get_cell") == 0)
     {
+        assert(CalcArgsNumber(node) == 2);
         if (CalcArgsNumber(node) != 2)
             return -1;
         fprintf(output_file, "get_cell\n");
     }
     else if (strcmp(VAL_FUNC(node), "build_cell") == 0)
     {
+        assert(CalcArgsNumber(node) == 3);
         if (CalcArgsNumber(node) != 3)
             return -1;
         fprintf(output_file, "build_cell\n");
